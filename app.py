3fa9c21a038d711ad2050389f2e0611a763d28b6
SamAshcroft <sam.ashcroft@hotmail.co.uk>
2025-06-27 13:54:30 +0100

    Added sentence

--- app.py
+++ app.py
@@ -6,3 +6,5 @@ dataframe = np.random.randn(10, 20)
 st.write("Hey here is my random table WOOP! Hello there")
 st.dataframe(dataframe)
 st.write("This working?")
+
+st.write("Ok let's see if this works!")
