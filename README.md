0cf49cc900e114cc12391cc55581830478b2bdbf
SamAshcroft <sam.ashcroft@hotmail.co.uk>
2025-06-30 10:41:26 +0100

    Adding github instructions

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # streamlit-hello-world
 Streamlit MVP
+
+Check VSCode is connected to GitHub
+git config --global user.name
+git config --global user.email
+git remote -v
--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-import streamlit as st
-import numpy as np
+# import streamlit as st
+# import numpy as np
 
-dataframe = np.random.randn(10, 20)
+# dataframe = np.random.randn(10, 20)
 
-st.write("Hey here is my random table WOOP! Hello there")
-st.dataframe(dataframe)
-st.write("This working?")
+# st.write("Hey here is my random table WOOP! Hello there")
+# st.dataframe(dataframe)
+# st.write("This working?")
 
-st.write("Ok let's see if this works!")
+# st.write("Ok let's see if this works!")
+
+# st.write("This coming through? Sure? Test")
+
