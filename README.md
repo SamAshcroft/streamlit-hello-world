bd13acbfc9c4f2ff2e12ee66e64ff40748ea5d3c
SamAshcroft <sam.ashcroft@hotmail.co.uk>
2025-06-30 12:10:34 +0100

    Text and image

--- README.md
+++
@@ -1,7 +0,0 @@
-# streamlit-hello-world
-Streamlit MVP
-
-Check VSCode is connected to GitHub
-git config --global user.name
-git config --global user.email
-git remote -v
--- app.py
+++ app.py
@@ -1,13 +1,40 @@
-# import streamlit as st
-# import numpy as np
+import streamlit as st
+from transformers import pipeline
+from PIL import Image
 
-# dataframe = np.random.randn(10, 20)
 
-# st.write("Hey here is my random table WOOP! Hello there")
-# st.dataframe(dataframe)
-# st.write("This working?")
+st.title("Reviews Sentiment Classifier")
 
-# st.write("Ok let's see if this works!")
+@st.cache_resource
+def load_text_model():
+    return pipeline(model="nlptown/bert-base-multilingual-uncased-sentiment")
 
-# st.write("This coming through? Sure? Test")
+text_classifier = load_text_model()
+
+
+text = st.text_input("Write your review here")
+
+if text:
+    output = text_classifier(text)
+    st.write("Classification output:")
+    st.json(output)
+
+# Image classification
+st.title("Image Classifier")
+
+@st.cache_resource
+def load_image_model():
+    return pipeline(model="microsoft/resnet-50")
+
+image_classifier = load_image_model()
+
+
+uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
+    result = image_classifier(image)
+    st.write(f"I am '**{100*result[0]['score']:.04f}**'% sure that this image contains a '**{result[0]['label'].capitalize()}**'")
+    st.write("Result:")
+    st.write(result)
 
